60bb7a6e05f819ee6339ebc5c1d2ed7a016c8162
Richard Lu <richlu1018@gmail.com>
2019-09-17 10:45:53 +0800

    Initial Commit

---
+++ FillTheTank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FillTheTank
+//
+//  Created by Richard Lu on 2019/9/17.
+//  Copyright © 2019 Richard Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
